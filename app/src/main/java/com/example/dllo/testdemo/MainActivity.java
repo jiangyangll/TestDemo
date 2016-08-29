79ecf75d99bdefb985bc9ba875fe3b73ae435e67
DravenForProgram <DravenForProgram@gmail.com>
2016-08-29 15:04:59 +0800

    edrfdsfsf

--- app/src/main/java/com/example/dllo/testdemo/MainActivity.java
+++ app/src/main/java/com/example/dllo/testdemo/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //写了代码
+        //asdfhosakhdfpoashjdfisadf
     }
 }
