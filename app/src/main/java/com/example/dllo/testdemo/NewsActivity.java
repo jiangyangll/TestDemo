4eead463a41a9cd1bdade2dccd125741c92d7473
dllo <dllo@jiangyang.local>
2016-08-29 17:02:48 +0800

    新建NewsActivity

---
+++ app/src/main/java/com/example/dllo/testdemo/NewsActivity.java
@@ -0,0 +1,16 @@
+package com.example.dllo.testdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/8/29.
+ */
+public class NewsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
